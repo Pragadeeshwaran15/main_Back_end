94d52bc9f472ed9f70ec5133607ae8678457cd7f
Pragadeeshwaran15 <pragadeshm501@gmail.com>
2024-03-20 21:22:18 +0530

    error fixed

--- src/controllers/adController.js
+++ src/controllers/adController.js
@@ -52,7 +52,7 @@ exports.deleteAd = catchAsyncError(async (req, res, next) => {
             new ErrorHandler(`User not found with this id ${req.params.id}`)
           );
         }
-        await user.deleteOne();
+        await Ad.deleteOne();
         res.status(200).json({
           success: true,
         }); 
@@ -62,33 +62,6 @@ exports.deleteAd = catchAsyncError(async (req, res, next) => {
         })
     }
   });
-//Update ad - /api/v1/update
-exports.updateAd = catchAsyncError(async (req, res, next) => {
-    let newUserData = {
-    };
-  
-    let adbanner;
-    let BASE_URL = process.env.BACKEND_URL;
-    if (process.env.NODE_ENV === "production") {
-      BASE_URL = `${req.protocol}://${req.get("host")}`;
-    }
-  
-    if (req.file) {
-      adbanner = `${BASE_URL}/uploads/Ads/${req.file.originalname}`;
-      newUserData = { ...newUserData, adbanner };
-    }
-  
-    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
-      new: true,
-      runValidators: true,
-    });
-  
-    res.status(200).json({
-      success: true,
-      user,
-    });
-  });
-
 exports.getAllimgaes = catchAsyncError(async (req, res, next) => {
     const images = await Ad.find();
     res.status(200).json({
