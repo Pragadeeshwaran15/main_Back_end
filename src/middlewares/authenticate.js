62381c2a43262bd1862eee837091d58800059ed2
Pragadeeshwaran15 <pragadeshm501@gmail.com>
2024-03-22 13:12:28 +0530

    token config

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,17 +3,28 @@ const User = require('../models/userModel')
 const catchAsyncError = require("./catchAsyncError");
 const jwt = require('jsonwebtoken');
 
-exports.isAuthenticatedUser = catchAsyncError( async (req, res, next) => {
-   const { token  }  = req.headers.authorization;
-   
-   if( !token ){
-        return next(new ErrorHandler('Login first to handle this resource', 401))
-   }
+exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+  const authorizationHeader = req.headers.authorization;
 
-   const decoded = jwt.verify(token, process.env.JWT_SECRET)
-   req.user = await User.findById(decoded.id)
-   next();
-})
+  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer')) {
+    return next(new ErrorHandler('Login first to handle this resource', 401));
+  }
+
+  const token = authorizationHeader.split(' ')[1];
+
+  if (!token) {
+    return next(new ErrorHandler('Invalid authorization token', 401));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (err) {
+    return next(new ErrorHandler('Unauthorized access', 401));
+  }
+});
+
 
 exports.authorizeRoles = (...roles) => {
    return  (req, res, next) => {
