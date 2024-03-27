66631b064ea1dd19265617731ef2f8c4d2806960
Pragadeeshwaran15 <pragadeshm501@gmail.com>
2024-03-27 07:30:23 +0530

    config

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,24 +1,29 @@
 const catchAsyncError = require('../middlewares/catchAsyncError');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.processPayment  = catchAsyncError(async(req, res, next) => {
+// Endpoint to process payment
+exports.processPayment = catchAsyncError(async (req, res, next) => {
+    // Create a PaymentIntent with Stripe
     const paymentIntent = await stripe.paymentIntents.create({
         amount: req.body.amount,
         currency: "usd",
         description: "TEST PAYMENT",
-        metadata: { integration_check: "accept_payment"},
+        metadata: { integration_check: "accept_payment" },
         shipping: req.body.shipping
-    })
+    });
 
+    // Send back the client secret of the PaymentIntent to the client
     res.status(200).json({
         success: true,
         client_secret: paymentIntent.client_secret
-    })
-})
+    });
+});
 
-exports.sendStripeApi  = catchAsyncError(async(req, res, next) => {
+// Endpoint to send the Stripe API key
+exports.sendStripeApi = catchAsyncError(async (req, res, next) => {
+    // Send the Stripe API key to the client
     res.status(200).json({
         stripeApiKey: process.env.STRIPE_API_KEY
-    })
-})
+    });
+});
 
